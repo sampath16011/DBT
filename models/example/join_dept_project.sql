863d463700233cda22b4c71e7f51f92472529eda
Bala Sampath Kumar <sampathkumar12062004@gmail.com>
2025-08-02 03:14:45 +0000

    join all tables

--- models/example/join_dept_project.sql
+++ models/example/join_dept_project.sql
@@ -1,3 +1,4 @@
-select d.department_name, p.project_id, p.project_name
+select e.first_name,d.department_name, p.project_id, p.project_name
 from {{ source("SUBQ", "projects") }} as p
 join {{ source("SUBQ", "departments") }} as d on p.department_id = d.department_id
+join {{source("SUBQ","employees")}} as e on e.department_id = d.department_id
