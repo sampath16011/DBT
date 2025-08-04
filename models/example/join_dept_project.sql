1a5a7b587a37fd976346650ad379dcd77a0a4ad1
Bala Sampath Kumar <sampathkumar12062004@gmail.com>
2025-08-04 06:28:37 +0000

    ADD TOP5 SALARY DROP JOIN

--- models/example/join_dept_project.sql
+++
@@ -1,4 +0,0 @@
-select e.first_name,d.department_name, p.project_id, p.project_name
-from {{ source("SUBQ", "projects") }} as p
-join {{ source("SUBQ", "departments") }} as d on p.department_id = d.department_id
-join {{source("SUBQ","employees")}} as e on e.department_id = d.department_id
---
+++ models/example/top5salary.sql
@@ -0,0 +1,7 @@
+SELECT *
+FROM (
+    SELECT *
+    FROM {{ source("SUBQ", "employees") }}
+    ORDER BY SALARY DESC
+    LIMIT 5
+) AS top_employees
